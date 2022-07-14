731c34f83def4f483dc14e8b715871587319b756
mtaqiyuddin <mtaqiyuddin101@gmail.com>
2022-07-14 09:20:24 +0700

    Methods

---
+++ Book.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndObjects
+{
+    public class Book
+    {
+        public string title;
+        public string author;
+        public int pages;
+        public Book(string aTitle,string aAuthor,int aPages)
+        {
+            title = aTitle;
+            author = aAuthor;
+            pages = aPages;
+        }
+
+        public bool BookMethod()
+        {
+            if (pages <= 400)
+            {
+                return true;
+            } return false;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+
+namespace ClassesAndObjects
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Book book1 = new Book("Harry Potter", "JKRowling",400);
+            Book book2 = new Book("Lord Angga", "Tolkeir", 700);
+            book2.title = "The Habbit";
+
+            Console.WriteLine(book1.BookMethod());
+            Console.WriteLine(book2.BookMethod());
+            Console.ReadLine();
+        }
+
+
+    }
+}
